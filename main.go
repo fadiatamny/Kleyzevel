d77a248111a41005d6fa16e2c4dceca91c25695a
Fadi Atamny <fadiatamny@gmail.com>
2023-12-03 02:25:52 +0200

    init server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Tuneless Treasures")
+	PORT := 3000
+	server := gin.Default()
+
+	server.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	server.Run(fmt.Sprintf(":%d", PORT))
 }
